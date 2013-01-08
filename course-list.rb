4f1c50021e1ae163448f5e4d74ee284de2ee6827
Al Johri <al.johri@gmail.com>
2013-01-08 09:38:27 -0600

    Cleaned up code. Added Shopping Cart.

--- course-list.rb
+++ course-list.rb
@@ -23,8 +23,6 @@ doc = page.parser
 
 numCourses = doc.xpath("//table[@id='ACE_STDNT_ENRL_SSV2$0']/tr/td[@valign='top']").size
 
-# puts numCourses
-
 i = 0
 doc.xpath("//table[@id='ACE_STDNT_ENRL_SSV2$0']/tr/td[@valign='top']/div/table/tr/td[@class='PAGROUPDIVIDER']").each{ |x|
 	i += 1
@@ -39,45 +37,5 @@ doc.xpath("//table[@id='ACE_STDNT_ENRL_SSV2$0']/tr/td[@valign='top']/div/table/t
 			doc.xpath("//span[@id='MTG_DATES$" + i.to_s + "']").text,
 		]
 }
-=begin
-numCourses.times { |i| 
- 	pp [
- 			doc.xpath("//span[@id='DERIVED_CLS_DTL_CLASS_NBR$" + i.to_s + "']").text,
- 			doc.xpath("//a[@id='MTG_SECTION$" + i.to_s + "']").text,
- 			doc.xpath("//span[@id='MTG_COMP$" + i.to_s + "']").text,
- 			doc.xpath("//span[@id='MTG_SCHED$" + i.to_s + "']").text,
- 			doc.xpath("//span[@id='MTG_LOC$" + i.to_s + "']").text,
- 			doc.xpath("//span[@id='DERIVED_CLS_DTL_SSR_INSTR_LONG$" + i.to_s + "']").text,
- 			doc.xpath("//span[@id='MTG_DATES$" + i.to_s + "']").text,
- 		]
- }
-=end
+
 puts "Time elapsed: #{Time.now - beginning} seconds."
-
-#pp page.search("div[@id='win0divSTDNT_ENRL_SSV2$0']").text
-#pp doc.xpath("//div[@id='win0divSTDNT_ENRL_SSV2$0']")
-
-# "//span[@id='DERIVED_CLS_DTL_CLASS_NBR$" + i.to_s +  "']"
-
-# doc.css("div[@id='win0divSTDNT_ENRL_SSV2$0'] tr").map{|x| 
-# 	puts
-# 		[
-# 			x.at("span[@id='DERIVED_CLS_DTL_CLASS_NBR$0'").text, 
-# 			x.at("a[@id='MTG_SECTION$0'").text,
-# 			x.at("span[@id='MTG_COMP$0'").text,
-# 			x.at("span[@id='MTG_SCHED$0'").text,
-# 			x.at("span[@id='MTG_LOC$0'").text,
-# 			x.at("span[@id='DERIVED_CLS_DTL_SSR_INSTR_LONG$0'").text,
-# 			x.at("span[@id='MTG_DATES$0'").text,
-# 		]
-# }
-
-# doc.css("div[@id='win0divSTDNT_ENRL_SSV2$0']/tr").map{|x| puts x.text}
-
-# win0divDERIVED_CLS_DTL_CLASS_NBR$0 -> DERIVED_CLS_DTL_CLASS_NBR$0
-# win0divMTG_SECTION$0 -> MTG_SECTION$0
-# win0divMTG_COMP$0 -> MTG_COMP$0
-# win0divMTG_SCHED$0 -> MTG_SCHED$0
-# win0divMTG_LOC$0 -> MTG_LOC$0
-# win0divDERIVED_CLS_DTL_SSR_INSTR_LONG$0 -> DERIVED_CLS_DTL_SSR_INSTR_LONG$0
-# win0divMTG_DATES$0 -> MTG_DATES$0
